1f1e8c728020f7538027802efd7356aaf53a0b15
Karanbir Singh <42942193+KaranJanjua@users.noreply.github.com>
2026-03-29 14:18:51 +1100

    add Employee model with data annotations

---
+++ Models/Employee.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MvcPayroll.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public bool IsActive { get; set;}
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+        [Required]
+        public string Department { get; set; }
+        [Required]
+        public string Office { get; set; }
+        [Required]
+        public string Location { get; set; }
+        [Required]
+        [Display(Name = "Annual Salary")]
+        [Range(1, 9999999)]
+        public decimal AnnualSalary { get; set; }
+        [Required]
+        [Display(Name = "Pay Frequency")]
+        public PayFrequency PayFrequency { get; set; }
+        [Required]
+        [Display(Name = "Bank Account Number")]
+        [StringLength(10, MinimumLength = 6)]
+        public string BankAccountNumber { get; set; }
+        [Required]
+        [EmailAddress]
+        [StringLength(125)]
+        public string Email { get; set; }
+    };
+
+    public enum PayFrequency 
+    {
+        Weekly = 1,
+        Fortnightly = 2,
+        Monthly = 3,
+    }
+}
